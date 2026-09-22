38fc955bea58301c3ee17242c34f3ad11cff2b32
cleanup <cleanup@example.invalid>
2026-09-22 00:46:09 +0000

    auto: 0.28.0 pin

--- pin.nix
+++ pin.nix
@@ -3,11 +3,7 @@
   version = "0.28.0";
   sourceRev = "700821e1364eaab0e8f21c538a2131719fec57bf";
   sourceHash = "sha256-p75qcm6eRMWaVD89xac2biu58hN2FcxW1QgNujlGxAA=";
-  dependencies = {
-    "comfyui-frontend-package" = "v1.45.21";
-    "comfyui-workflow-templates" = "v0.11.9";
-    "comfyui-embedded-docs" = "v0.5.8";
-    "comfy-kitchen" = "v0.2.20";
-    "comfy-aimdo" = "v0.4.10";
-  };
+  requirementsHash = "02a05aece14931019ce30141c5a228ac4bdbec2d288764afb3254bbcdcbdb614";
+  wheelManifestHash = "0f6f7b3e802ff113097361bec297bc0d2e83d62d9cb909929b8961210cbcec63";
+  artifactFingerprint = "20d5e9e6bb1521132f2f40685802add08235e96141f4a9b2ee9181c4e35ec60f";
 }
