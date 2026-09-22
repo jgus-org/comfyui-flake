7d7f96e77afff4e63cc384ae498e0c5b2461cd29
cleanup <cleanup@example.invalid>
2026-09-22 00:38:07 +0000

    overlay owned files from 809858900707bb598f3d7ba3e4a1c1b31b682508

--- pin.nix
+++ pin.nix
@@ -1,9 +1,13 @@
 # Auto-managed by `nix run .#update-version`. Manual edits will be overwritten by the next bump.
 {
-  version = "0.37.0";
-  sourceRev = "73c9bad4d21e7addbe1d13bc92eee0f1431b017d";
-  sourceHash = "sha256-hfpoQsu8xzKHCy2Qqw2BMGsorwizJEuhKXWjUUJzTHs=";
-  requirementsHash = "9ecd1541bb995949c89733cea34c3b870800fad92db43d668c1b08cacc190920";
-  wheelManifestHash = "9f9c444f8461dd1a757234a4d04009d0d29ab140c60f6e8a3cfc7491527d320f";
-  artifactFingerprint = "ae3c91f3c2bb9d70ac0f242d917c7f7342cd4b28d52856ae7a070e9a2636ac8a";
+  version = "0.33.1";
+  sourceRev = "72865f4f27eaf5396f8f36370e0a2be3a9a090ee";
+  sourceHash = "sha256-r6amApYXBjfE8+UvhiJ/7VcUibd4kd5i+JLPXxo8H9w=";
+  dependencies = {
+    "comfyui-frontend-package" = "v1.48.7";
+    "comfyui-workflow-templates" = "v0.11.41";
+    "comfyui-embedded-docs" = "v0.5.9";
+    "comfy-kitchen" = "v0.2.31";
+    "comfy-aimdo" = "v0.4.13";
+  };
 }
